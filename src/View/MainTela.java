030498f3fa707138c161f79771cd61f9a56d0a9d
LuizLoch <luizloch242@gmail.com>
2025-11-30 22:24:25 -0300

    Na finalera

--- src/View/MainTela.java
+++ src/View/MainTela.java
@@ -22,10 +22,6 @@ public class MainTela extends javax.swing.JFrame {
         jPanel4 = new javax.swing.JPanel();
         b_cliente = new javax.swing.JButton();
         b_produto = new javax.swing.JButton();
-        b_pedidos = new javax.swing.JButton();
-        b_categoria = new javax.swing.JButton();
-        b_inventario = new javax.swing.JButton();
-        b_dashboard = new javax.swing.JButton();
         jPanel3 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
 
@@ -54,46 +50,6 @@ public class MainTela extends javax.swing.JFrame {
             }
         });
 
-        b_pedidos.setBackground(new java.awt.Color(204, 204, 204));
-        b_pedidos.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        b_pedidos.setForeground(new java.awt.Color(32, 32, 32));
-        b_pedidos.setText("Pedidos");
-        b_pedidos.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                b_pedidosActionPerformed(evt);
-            }
-        });
-
-        b_categoria.setBackground(new java.awt.Color(204, 204, 204));
-        b_categoria.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        b_categoria.setForeground(new java.awt.Color(32, 32, 32));
-        b_categoria.setText("Categoria");
-        b_categoria.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                b_categoriaActionPerformed(evt);
-            }
-        });
-
-        b_inventario.setBackground(new java.awt.Color(204, 204, 204));
-        b_inventario.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        b_inventario.setForeground(new java.awt.Color(32, 32, 32));
-        b_inventario.setText("Inventário");
-        b_inventario.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                b_inventarioActionPerformed(evt);
-            }
-        });
-
-        b_dashboard.setBackground(new java.awt.Color(204, 204, 204));
-        b_dashboard.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        b_dashboard.setForeground(new java.awt.Color(32, 32, 32));
-        b_dashboard.setText("Dashboard");
-        b_dashboard.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                b_dashboardActionPerformed(evt);
-            }
-        });
-
         javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
         jPanel4.setLayout(jPanel4Layout);
         jPanel4Layout.setHorizontalGroup(
@@ -102,11 +58,7 @@ public class MainTela extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(b_cliente, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(b_produto, javax.swing.GroupLayout.DEFAULT_SIZE, 138, Short.MAX_VALUE)
-                    .addComponent(b_pedidos, javax.swing.GroupLayout.DEFAULT_SIZE, 138, Short.MAX_VALUE)
-                    .addComponent(b_categoria, javax.swing.GroupLayout.DEFAULT_SIZE, 138, Short.MAX_VALUE)
-                    .addComponent(b_inventario, javax.swing.GroupLayout.DEFAULT_SIZE, 138, Short.MAX_VALUE)
-                    .addComponent(b_dashboard, javax.swing.GroupLayout.DEFAULT_SIZE, 138, Short.MAX_VALUE))
+                    .addComponent(b_produto, javax.swing.GroupLayout.DEFAULT_SIZE, 138, Short.MAX_VALUE))
                 .addContainerGap())
         );
         jPanel4Layout.setVerticalGroup(
@@ -116,15 +68,7 @@ public class MainTela extends javax.swing.JFrame {
                 .addComponent(b_cliente)
                 .addGap(60, 60, 60)
                 .addComponent(b_produto)
-                .addGap(60, 60, 60)
-                .addComponent(b_pedidos)
-                .addGap(60, 60, 60)
-                .addComponent(b_categoria)
-                .addGap(60, 60, 60)
-                .addComponent(b_inventario)
-                .addGap(60, 60, 60)
-                .addComponent(b_dashboard)
-                .addGap(30, 30, 30))
+                .addGap(410, 410, 410))
         );
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
@@ -184,29 +128,15 @@ public class MainTela extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void b_clienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b_clienteActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_b_clienteActionPerformed
-
     private void b_produtoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b_produtoActionPerformed
-        // TODO add your handling code here:
+        ProdutoEscTela telap = new ProdutoEscTela();
+        telap.setVisible(true);
     }//GEN-LAST:event_b_produtoActionPerformed
 
-    private void b_pedidosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b_pedidosActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_b_pedidosActionPerformed
-
-    private void b_categoriaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b_categoriaActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_b_categoriaActionPerformed
-
-    private void b_inventarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b_inventarioActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_b_inventarioActionPerformed
-
-    private void b_dashboardActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b_dashboardActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_b_dashboardActionPerformed
+    private void b_clienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b_clienteActionPerformed
+        ClienteEscTela telac = new ClienteEscTela();
+        telac.setVisible(true);
+    }//GEN-LAST:event_b_clienteActionPerformed
 
     /**
      * @param args the command line arguments
@@ -234,11 +164,7 @@ public class MainTela extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton b_categoria;
     private javax.swing.JButton b_cliente;
-    private javax.swing.JButton b_dashboard;
-    private javax.swing.JButton b_inventario;
-    private javax.swing.JButton b_pedidos;
     private javax.swing.JButton b_produto;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel2;
